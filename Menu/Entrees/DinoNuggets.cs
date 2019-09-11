6491771c9c767db69d3da864f3d23e48aa584134
milan <milan@CIS-RDS4>
2019-09-11 15:03:34 -0500

    Added all finished entree files to new repo

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+
+        private uint numNuggets = 6;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>();
+                for (int i=0; i<numNuggets; i++)
+                {
+                    ingredients.Add("Chicken Nugget");
+                }
+                return ingredients;
+            }
+        }
+
+        public DinoNuggets()
+        {
+            this.Price = 4.25;
+            this.Calories = 59*numNuggets;
+        }
+
+        public void AddNugget()
+        {
+            this.numNuggets++;
+        }
+    }
+}
